9a40d8c96808cb46f5ffe4953f8096041e21234b
yichchia <yichchia@163.com>
2017-06-03 10:07:27 +0800

    first commmit by ash

--- dailyfresh/df_user/urls.py
+++ dailyfresh/df_user/urls.py
@@ -4,7 +4,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns =[
-    url(r'^$', views.index),
-    url(r'^login$', views.login),
-    url(r'^register$', views.register)
+    url(r'^login/$', views.login),
+    url(r'^register/$', views.register),
+    url(r'^login_handle/$', views.login_handle),
+    url(r'^register_handle/$', views.register_handle),
+    url(r'^info/$', views.info),
+    url(r'^order/$', views.order),
+    url(r'^site_list', views.site_list),
 ]
